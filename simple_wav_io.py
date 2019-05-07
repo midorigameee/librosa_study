4b054f4253469569f790c1268a262d04069b2015
shin-2D7 <xqbodp727@gmail.com>
2019-05-07 19:54:07 +0900

    edit simple_wav_io.py

--- simple_wav_io.py
+++ simple_wav_io.py
@@ -1,9 +1,15 @@
 import librosa
 
-filename = "data_1s.wav"
 
-
-wav, sr = librosa.load(path=filename)
-
+filename = "test.wav"
 savename = filename[:-4] + "_output.wav"
+sr = 44100
+
+# サンプリング周波数は引数srで指定しないと
+# 勝手に22,050Hzになる
+
+# wavを読み込む関数
+wav, sr = librosa.load(path=filename, sr=sr)
+
+# wavを出力する関数
 librosa.output.write_wav(y=wav, path=savename, sr=sr)
